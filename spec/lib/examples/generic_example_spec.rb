f0f6e72805118c03eb2ac4c24fb53da2544beb72
Ben Falk <benjamin.falk@yahoo.com>
2024-05-03 07:28:02 -0400

    add generic example

---
+++ spec/lib/examples/generic_example_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+Event = Scheming.generic do |(subject, data)|
+  Object(
+    subject:,
+    data:,
+    event_key: String,
+    seq_no: Integer
+  )
+end
+
+SubjectB = Scheming.object do
+  attribute :id, String
+  attribute :consumer_id, String
+end
+
+SubjectA = Scheming.object do
+  attribute :id, Integer
+  attribute :colors, Array(String)
+end
+
+BaseData = Scheming.object do
+  attribute :created_at, Integer
+end
+
+EventA = Event[SubjectA, BaseData]
+EventB = Event[SubjectB, BaseData]
+
+RSpec.describe do
+  it do
+    expect(Scheming::Schema.json(EventA)).to eq(
+      type: 'object',
+      additionalProperties: false,
+      required: %i[subject data event_key seq_no],
+      properties: {
+        subject: {
+          type: 'object',
+          additionalProperties: false,
+          required: %i[id colors],
+          properties: {
+            id: { type: 'integer' },
+            colors: {
+              type: 'array',
+              items: { type: 'string' }
+            }
+          }
+        },
+        data: {
+          type: 'object',
+          additionalProperties: false,
+          required: %i[created_at],
+          properties: {
+            created_at: { type: 'integer' }
+          }
+        },
+        event_key: { type: 'string' },
+        seq_no: { type: 'integer' }
+      }
+    )
+
+    expect(Scheming::Schema.json(EventB)).to eq(
+      type: 'object',
+      additionalProperties: false,
+      required: %i[subject data event_key seq_no],
+      properties: {
+        subject: {
+          type: 'object',
+          additionalProperties: false,
+          required: %i[id consumer_id],
+          properties: {
+            id: { type: 'string' },
+            consumer_id: { type: 'string' }
+          }
+        },
+        data: {
+          type: 'object',
+          additionalProperties: false,
+          required: %i[created_at],
+          properties: {
+            created_at: { type: 'integer' }
+          }
+        },
+        event_key: { type: 'string' },
+        seq_no: { type: 'integer' }
+      }
+    )
+  end
+end
